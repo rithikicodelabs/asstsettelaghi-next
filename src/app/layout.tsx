5f94a9a2ce6226ac339eae12ece0aba680150828
rithik <rithik.rajkumar@icodelabs.co>
2025-08-07 11:18:18 +0530

    Updated project name, added ESLint and Prettier configurations, and modified TypeScript settings. Replaced global styles with design tokens from Italia and streamlined CSS structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
 
 const geistSans = Geist({
